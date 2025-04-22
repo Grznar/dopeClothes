ef748c6e8d5cb6e3da5a4e371a88d5d99e508ce4
Jakub Černý <AsKronak@seznam.cz>
2025-04-22 09:08:08 +0200

    cartId

--- dopeClothes.Client/src/views/ShoppingCarts_Orders/ShoppingCart.vue
+++ dopeClothes.Client/src/views/ShoppingCarts_Orders/ShoppingCart.vue
@@ -99,7 +99,7 @@ async function toCheckout() {
     console.log(cartId);
     router.push({
       name: APP_ROUTE_NAMES.ORDER,
-      params: { id: cartId },
+      params: { cartId: cartId },
     });
   } catch (error) {
     console.log(error.message);
